4e4ef6ed69ca1754516aff8e31664354c211ae21
RanGua <ikunbeautiful@gmail.com>
2026-01-15 16:59:58 -0800

    Update script.js

--- script.js
+++ script.js
@@ -272,7 +272,7 @@ function attachDragEvents() {
 }
 
 // ---------- Tab Management ----------
-function newTab(url = "https://proxy.jimmyqrg.com/default/") {
+function newTab(url = "https://proxy.schooloqy.com/default/") {
     const id = Date.now();
     const iframe = document.createElement("iframe");
     
@@ -815,7 +815,7 @@ document.getElementById("reload").onclick = () => {
 };
 
 document.getElementById("home").onclick = () => {
-    navigate("https://proxy.jimmyqrg.com/default/");
+    navigate("https://proxy.schooloqy.com/default/");
 };
 
 document.getElementById("bookmark").onclick = () => {
